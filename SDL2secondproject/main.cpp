205fdb76d45e10c35654171924cfda05bfaa42da
autentico.jason <autentico.jason@gmail.com>
2018-06-05 18:29:09 +0800

    ClassAdded

--- SDL2secondproject/main.cpp
+++ SDL2secondproject/main.cpp
@@ -1,31 +1,25 @@
-#include <SDL.h>
+//#include <SDL.h>
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
+//SDL_Window* g_pWindow = 0;
+//SDL_Renderer* g_pRenderer = 0;
 
-int main(int argc, char* args[])
+
+
+
+bool g_bRunning = true;
+
+int main()
 {
-		if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) 
-		{
-				g_pWindow = SDL_CreateWindow("this is a window",
-					SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
-				640,480,SDL_WINDOW_SHOWN);
-				if (g_pWindow != 0)
-				{
-					g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-				}
-		}
-		else
-		{		
+	init();
+
+	while (g_bRunning)
+	{
+		handleEvents();
+		update();
+		render();
+	}
+	clean();
 		
-			return 1;
-	    }
-	//this is latest commit
-			SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-			SDL_RenderClear(g_pRenderer);
-			SDL_RenderPresent(g_pRenderer);
-			SDL_Delay(5000);
-			SDL_Quit();
 		
 		return 0;
 }
