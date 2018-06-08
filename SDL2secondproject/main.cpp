def8af09c90b5eec519e6deadc9f3bd9f14222e2
autentico.jason <autentico.jason@gmail.com>
2018-06-08 15:36:42 +0800

    dfd

--- SDL2secondproject/main.cpp
+++ SDL2secondproject/main.cpp
@@ -1,5 +1,3 @@
-
-	
 #include <SDL.h>
 
 int main(int argc, char ** argv)
@@ -10,9 +8,6 @@ int main(int argc, char ** argv)
 	SDL_Event event;
 	int x = 288;
 	int y = 208;
-	int xvel = 0;
-	int yvel = 0;
-	
 
 	// init SDL
 
@@ -32,7 +27,7 @@ int main(int argc, char ** argv)
 
 	while (!quit)
 	{
-		SDL_Delay(5);
+		SDL_Delay(3);
 		SDL_PollEvent(&event);
 
 		switch (event.type)
@@ -40,60 +35,27 @@ int main(int argc, char ** argv)
 		case SDL_QUIT:
 			quit = true;
 			break;
-
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.sym)
 			{
-			case SDLK_LEFT:  xvel = -1; break;
-			case SDLK_RIGHT: xvel =  1; break;
-			case SDLK_UP:    yvel = -1; break;
-			case SDLK_DOWN:  yvel = 1; break;
-			default:
-				break;
-
-
+			case SDLK_LEFT:  x--; break;
+			case SDLK_RIGHT: x++; break;
+			case SDLK_UP:    y--; break;
+			case SDLK_DOWN:  y++; break;
 			}
 
-		//	break;
-		case SDL_KEYUP:
-			switch (event.key.keysym.sym)
-			{
-			case SDLK_LEFT:
-				if (xvel > 0)
-					xvel = 0;
-				break;
-			case SDLK_RIGHT:
-				if (xvel < 0)
-					xvel = 0;
-				break;
-			case SDLK_UP:
-				if (yvel > 0)
-					yvel = 0;
-				break;
-			case SDLK_DOWN:
-				if (yvel < 0)
-					yvel = 0;
-				break;
-			default:
-				break;
-			}
 			break;
-		  default:
-			break;
+
 		}
-		x += xvel;
-		y += yvel;
-
-		
-
 
 		SDL_Rect dstrect = { x, y, 64, 64 };
 
 		SDL_RenderClear(renderer);
 		SDL_RenderCopy(renderer, texture, NULL, &dstrect);
 		SDL_RenderPresent(renderer);
+
 	}
- 
+
 
 	// cleanup SDL
 
@@ -101,8 +63,6 @@ int main(int argc, char ** argv)
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
-
 
 	return 0;
 }
